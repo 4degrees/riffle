5b6a2b1a44a0ce21afef09795971cb39e02bf676
Christoph Buelter <c.buelter@arcor.de>
2014-08-26 19:16:08 +0200

    Move shortcut setup to dedicated function.

--- source/riffle/browser.py
+++ source/riffle/browser.py
@@ -43,9 +43,6 @@ class FilesystemBrowser(QtGui.QDialog):
 
         self._upButton = QtGui.QToolButton()
         self._upButton.setIcon(QtGui.QIcon(':riffle/icon/up'))
-        self._upShortcut = QtGui.QShortcut(
-            QtGui.QKeySequence("Backspace"), self
-        )
         self._headerLayout.addWidget(self._upButton)
 
         self.layout().addLayout(self._headerLayout)
@@ -104,6 +101,8 @@ class FilesystemBrowser(QtGui.QDialog):
         self._acceptButton.clicked.connect(self.accept)
         self._cancelButton.clicked.connect(self.reject)
 
+        self._configureShortcuts()
+
         self.setLocation(self._root)
 
         self._filesystemWidget.horizontalHeader().setResizeMode(
@@ -114,7 +113,6 @@ class FilesystemBrowser(QtGui.QDialog):
         )
 
         self._upButton.clicked.connect(self._onNavigateUpButtonClicked)
-        self._upShortcut.activated.connect(self._onNavigateUpButtonClicked)
         self._locationWidget.currentIndexChanged.connect(
             self._onNavigate
         )
@@ -122,6 +120,14 @@ class FilesystemBrowser(QtGui.QDialog):
         self._filesystemWidget.activated.connect(self._onActivateItem)
         selectionModel = self._filesystemWidget.selectionModel()
         selectionModel.currentRowChanged.connect(self._onSelectItem)
+
+    def _configureShortcuts(self):
+        '''Add keyboard shortcuts to navigate the filesystem.'''
+        self._upShortcut = QtGui.QShortcut(
+            QtGui.QKeySequence("Backspace"), self
+        )
+        self._upShortcut.setAutoRepeat(False)
+        self._upShortcut.activated.connect(self._onNavigateUpButtonClicked)
 
     def _onActivateItem(self, index):
         '''Handle activation of item in listing.'''
